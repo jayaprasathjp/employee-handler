e0840c88adc380e883f80645563d8ac6f2c63f8f
jayaprasathjp <jayaprasathjp44@gmail.com>
2024-05-23 22:10:43 +0530

    final up

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -2,6 +2,108 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
+    await prisma.employee.createMany({
+        data: [
+          {
+            id: 5,
+            name: 'abeesh',
+            dob: new Date('2024-02-15'),
+            department: 'Database Administrator',
+            position: 'Employee',
+            address: 'wwww',
+            email: 'jayaprasathjp44@gmail.com',
+            phone: '+919894432092',
+            image: 'Hari.jpg',
+            salary: '087987796',
+            city: 'Namakkal',
+            state: 'Tamil Nadu'
+          },
+          {
+            id: 6,
+            name: 'JAYAPRASATH K',
+            dob: new Date('2024-05-22'),
+            department: 'Software Architect',
+            position: 'Manager',
+            address: '2/117, Kunnamalai, Paramathi Velur(Taluk)',
+            email: 'jayaprasathjp44@gmail.com',
+            phone: '+919894432092',
+            image: 'a2.jpg',
+            salary: '87978',
+            city: 'Namakkal',
+            state: 'Tamil Nadu'
+          },
+          {
+            id: 8,
+            name: 'JAYAPRASATH K',
+            dob: new Date('2024-05-29'),
+            department: 'Software Architect',
+            position: 'Manager',
+            address: '2/117, Kunnamalai, Paramathi Velur(Taluk)',
+            email: 'jayaprasathjp44@gmail.com',
+            phone: '+919894432092',
+            image: 'HRMS - Complete Guide on Managing Your Human Resource.jpeg',
+            salary: '987',
+            city: 'Namakkal',
+            state: 'Tamil Nadu'
+          },
+          {
+            id: 101,
+            name: 'Bob Smith',
+            dob: new Date('1985-07-21'),
+            department: 'IT',
+            position: 'Developer',
+            address: '456 Elm Street',
+            email: 'bob.smith@example.com',
+            phone: '234-567-8901',
+            image: 'path/to/image2.jpg',
+            salary: '60000',
+            city: 'San Francisco',
+            state: 'CA'
+          },
+          {
+            id: 102,
+            name: 'Carol Taylor',
+            dob: new Date('1979-12-29'),
+            department: 'Marketing',
+            position: 'Manager',
+            address: '789 Oak Street',
+            email: 'carol.taylor@example.com',
+            phone: '345-678-9012',
+            image: 'path/to/image3.jpg',
+            salary: '70000',
+            city: 'Chicago',
+            state: 'IL'
+          },
+          {
+            id: 103,
+            name: 'David Lee',
+            dob: new Date('1990-04-04'),
+            department: 'Sales',
+            position: 'Salesperson',
+            address: '101 Pine Street',
+            email: 'david.lee@example.com',
+            phone: '456-789-0123',
+            image: 'path/to/image4.jpg',
+            salary: '55000',
+            city: 'Boston',
+            state: 'MA'
+          },
+          {
+            id: 104,
+            name: 'JAYAPRASATH K',
+            dob: new Date('2024-05-22'),
+            department: 'Software Architect',
+            position: 'Employee',
+            address: '2/117, Kunnamalai, Paramathi Velur(Taluk)',
+            email: 'jayaprasathjp44@gmail.com',
+            phone: '+919894432092',
+            image: '927621BIT006.jpg',
+            salary: '54645',
+            city: 'Namakkal',
+            state: 'Tamil Nadu'
+          },
+        ],
+      });
     const data = [
         {id:100, name: "Alice Johnson", dob: new Date(1980, 4, 15), department: "Finance", position: "Accountant", address: "123 Maple Street", email: "alice.johnson@example.com", phone: "123-456-7890", image: "path/to/image1.jpg", salary: "50000", city: "New York", state: "NY" },
         {id:101, name: "Bob Smith", dob: new Date(1985, 6, 22), department: "IT", position: "Developer", address: "456 Elm Street", email: "bob.smith@example.com", phone: "234-567-8901", image: "path/to/image2.jpg", salary: "60000", city: "San Francisco", state: "CA" },
